392fcb8ada50c24fd3fb8a7463a0376eb6d3a809
Chikkala Sanjan <97677252+sanjangithub@users.noreply.github.com>
2024-12-26 17:33:03 +0530

    Update QuizappApplicationTests.java

--- src/test/java/com/sanjan/quizapp/QuizappApplicationTests.java
+++ src/test/java/com/sanjan/quizapp/QuizappApplicationTests.java
@@ -3,7 +3,7 @@ package com.sanjan.quizapp;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest sanjan
 class QuizappApplicationTests {
 
 	@Test
